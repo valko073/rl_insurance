e2093f4f352d9d5f09958c4d14dc4588bf49815a
Valentin <vakoch@uni-osnabrueck.de>
2019-10-17 17:47:34 +0200

    added results fetch_experiments

--- parameter_search.py
+++ parameter_search.py
@@ -3,7 +3,7 @@ import multiprocessing
 import os
 import sys
 
-alphas = np.geomspace(0.00001, 1, endpoint=False, num=20)
+alphas = np.geomspace(0.00001, 0.5, endpoint=False, num=10)
 replay_len = [1, 10, 100, 250, 500, 1000]
 # replay_len = [1, 10]
 target_model_updates = [.09, .5, .9, 10, 100, 500, 1000]
@@ -13,7 +13,7 @@ def worker(alph):
     print(os.path.dirname(sys.executable))
     for rep_len in replay_len:
         for tar_mod_up in target_model_updates:
-            os.system("run_insurance.py --comet --num_insurances=3 --num_agents=3 --num_steps=2 "+
+            os.system("python3 run_insurance.py --comet --num_insurances=3 --num_agents=3 --num_steps=100000 "+
                       "--learning_rate={} --memory_limit={} --target_model_update={}".format(alph, rep_len, tar_mod_up))
 
 
