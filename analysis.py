d592d6a963b454ff7fef9ae29cea2fcde145f4d4
Valentin <vakoch@uni-osnabrueck.de>
2019-11-06 11:34:57 +0100

    updated analysis

--- analysis.py
+++ analysis.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from mpl_toolkits.mplot3d import Axes3D
+plt.rcParams.update({'font.size': 22})
+
 
 with open('dumps/correlations.p', 'rb') as f:
     correlations = pickle.load(f)
@@ -23,6 +25,15 @@ with open('dumps/lyap_intra.p', 'rb') as f:
     
 params = np.array(params).astype(np.float)
 
+to_remove = [20,28]
+for i in to_remove:
+    del correlations[i]
+    del ids[i]
+    del lyap_inter[i]
+    del neg_intra[i]
+    del params[i]
+    del lyap_intra[i]
+
 correlations = correlations[345:]
 costs = costs[345:]
 ids = ids[345:]
@@ -30,7 +41,8 @@ lyap_inter = lyap_inter[345:]
 neg_intra = neg_intra[345:]
 params = params[345:]
 usages = usages[345:]
-layp_intra = lyap_intra[345:]
+lyap_intra = lyap_intra[345:]
+
 
 lyap_inter_min = np.mean(np.abs(np.array(lyap_inter).astype(np.float)), axis=1)
 lyap_inter_min = np.min(np.abs(np.array(lyap_inter).astype(np.float)), axis=1)
@@ -46,11 +58,17 @@ lyap_abs_mean = np.mean(np.mean(np.abs(lyap_intra),axis=1),axis=1)
 
 sorted_lyap = (lyap_abs_mean - lyap_intra_min).argsort()
 
-s = np.argsort(lyap_intra_min).argsort() # falsch rum, groß gut
-t = np.max(np.abs(correlations),axis=1).argsort().argsort() # falsch rum, groß gut
+s = np.argsort(lyap_intra_min)[::-1].argsort() # falsch rum, groß gut
+t = np.max(np.abs(correlations),axis=1).argsort()[::-1].argsort() # falsch rum, groß gut
 #u = np.min(np.array(lyap_inter).astype(np.float), axis=1).argsort().argsort()[::-1]
 tmp = np.mean(np.array([s,t]),axis=0).argsort()
+
+
+
 to_plot = np.mean(np.array([s,t]),axis=0)
+
+zipped = list(zip(params[:,0].astype(str),params[:,2].astype(str),to_plot))
+
 print(ids[tmp])
 
 plt.figure()
@@ -159,18 +177,6 @@ plt.subplot(1,3,3)
 plt.scatter(params[:,2], lyap_intra_min)
 plt.xlabel('alpha')
 
-fig = plt.figure()
-plt.subplot(1,2,1)
-x=np.array(sorted(zip(params[:,0].astype(str),to_plot,params[:,2]),key=lambda x:float(x[0])))
-img = plt.scatter(x[:,0],x[:,1].astype(float),c=x[:,2].astype(float),cmap='jet',norm=matplotlib.colors.Normalize(0,0.3),s=100)
-fig.colorbar(img)
-plt.xlabel('target_model_update')
-plt.ylabel('Percentage of episodes w/ negative lyap exp')
-plt.subplot(1,2,2)
-x=np.array(sorted(zip(params[:,2].astype(str),to_plot,params[:,0]),key=lambda x:float(x[0])))
-img = plt.scatter(x[:,0],x[:,1].astype(float),c=x[:,2].astype(float),cmap='jet',s=100)
-fig.colorbar(img)
-plt.xlabel('alpha')
 
 plt.figure()
 plt.subplot(1,3,1)
@@ -198,3 +204,30 @@ ax = fig.add_subplot(111, projection='3d')
 img = ax.scatter(params[:,0],params[:,2], tmp, c='b')
 ax.set_xlabel('Model_update'); ax.set_ylabel('Learning_rate');
 plt.title('3d plot')
+
+
+plt.figure()
+plt.subplot(2,1,1)
+sorted_to_plot = np.array(sorted(zipped,key=lambda x:x[0].astype(float)))
+plt.scatter(sorted_to_plot[:,0],sorted_to_plot[:,2].astype(float),c=sorted_to_plot[:,1].astype(float),s=75)
+plt.colorbar().set_label('Learning rate')
+plt.xlabel('target_model_update')
+plt.ylabel('Ranking')
+plt.subplot(2,1,2)
+sorted_to_plot = np.array(sorted(zipped,key=lambda x:x[1].astype(float)))
+plt.scatter(sorted_to_plot[:,1],sorted_to_plot[:,2].astype(float),c=sorted_to_plot[:,0].astype(float),s=75)
+plt.xlabel('Learning rate')
+plt.colorbar().set_label('Target model update')
+plt.xticks(sorted_to_plot[:,1],[round(float(x),5) for x in sorted_to_plot[:,1]],rotation='vertical')
+plt.suptitle('Ranking of experiments (low -> interesting dynamics)')
+
+
+x = np.arange(0,4*np.pi,0.1)   # start,stop,step
+y = np.sin(x)
+z = np.cos(x)
+plt.figure()
+plt.plot(x,y,x,z)
+plt.xticks([], [])
+plt.yticks([], [])
+plt.xlabel('Timestep t')
+plt.legend(['Insurance price','Insurance usage'])
